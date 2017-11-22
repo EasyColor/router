99de4d41cceb08f7c246e8e0ffc3f32f9b6f7512
Vreixo Gonzalez <vreixo@gmail.com>
2017-11-22 18:42:23 +0300

    Added test data

---
+++ test-data.sql
@@ -0,0 +1,32 @@
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,2,'green','red','',24,19);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,2,'green','red','blue',1,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,2,'red','','',1,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,3,'blue','green','',17,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,3,'red','','',1,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,4,'green','red','',8,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,5,'green','red','',18,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,6,'green','blue','red',2,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,7,'green','red','',3,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,8,'green','red','',1,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,9,'blue','green','red',3,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,10,'blue','red','',8,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,11,'blue','red','',10,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,12,'green','red','blue',8,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,13,'blue','','',6,1);
+INSERT INTO backend_router.route (routeorigin,routedestination,firstcolor,secondcolor,thirdcolor,assigned,capacity) VALUES (
+1,14,'blue','red','',0,1);
